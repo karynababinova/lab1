e3799f512313f2b080c939324c3e90f58509f8b0
sasha <silencesash@gmail.com>
2025-10-02 20:46:56 +0300

    experiment final ver

--- experiment.h
+++ experiment.h
@@ -1,9 +1,11 @@
 #pragma once
 #include <vector>
+#include <Board.h>
+#include <RandomCell.h>
 
 using namespace std;
 
 void experiment(int n, int m, int trial);
-vector <int> singleExperiment(int n, int m);
+vector <int> singleExperiment(int n, int m, Board& board, RandomCell& rc);
 double averageInVector(const vector<double>& v);
 pair <double, double> multipleExperiments(int n, int m, int repeat);
