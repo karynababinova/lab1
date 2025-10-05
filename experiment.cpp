a898e8ddd44f1f9bc8f554eddd20d8084de6da76
sasha <silencesash@gmail.com>
2025-10-05 19:53:08 +0300

    added randomness check

--- experiment.cpp
+++ experiment.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-vector <int> singleExperiment(int n, int m, Board& board, RandomCell& rc){
+vector <int> singleExperiment(int n, int m, Board& board, RandomCell& rc) {
     for (int t = 0; t < m; t++) {
         pair<int, int> xy = rc();
         board.incrementCell(xy);
@@ -40,12 +40,42 @@ pair <double, double> multipleExperiments(int n, int m, int repeat) {
     return { avg_avg, med_avg };
 }
 
+void checkRandomness(int n, int m) {
+    Board board(n);
+    RandomCell rc(n);
+
+    for (int t = 0; t < m; t++) {
+        pair<int, int> xy = rc();
+        board.incrementCell(xy);
+    }
+
+    auto counts = board.getAllMultiplicities();
+    cout << "Multiplicities for m = " << m << ": ";
+    const int max_display = 10;
+    if (counts.size() <= max_display) {
+        for (int c : counts)
+            cout << c << " ";
+    }
+    else {
+        for (int i = 0; i < 5; i++)
+            cout << counts[i] << " ";
+        cout << "... ";
+        for (int i = counts.size() - 5; i < counts.size(); i++)
+            cout << counts[i] << " ";
+    }
+    cout << endl;
+}
+
 void experiment(int n, int m, int repeat) {
     vector<int> ms = { m / 5, m / 4, m / 3, m / 2, m, m * 2, m * 3, m * 4, m * 5 };
-    cout << "m\tExpected\tAverage\tMedian" << endl;
+    cout << "m/Expected/Average/Median" << endl;
     for (int current_m : ms) {
         auto [avg, med] = multipleExperiments(n, current_m, repeat);
         double exp_avg = (double)current_m / (n * n);
         cout << current_m << "\t" << exp_avg << "\t" << avg << "\t" << med << endl;
+    }
+    cout << "Randomness check: " << endl;
+    for (int current_m : ms) {
+        checkRandomness(n, current_m);
     }
 }
--- experiment.h
+++ experiment.h
@@ -5,7 +5,8 @@
 
 using namespace std;
 
-void experiment(int n, int m, int trial);
-vector <int> singleExperiment(int n, int m);
+void experiment(int n, int m, int repeat);
+vector <int> singleExperiment(int n, int m, Board& board, RandomCell& rc);
 double averageInVector(const vector<double>& v);
 pair <double, double> multipleExperiments(int n, int m, int repeat);
+void checkRandomness(int n, int m);
